6f5a390085d791f26359219b1660998d5b957ac8
Revanth <revanthreddypedaballi@gmail.com>
2025-07-31 12:50:49 -0500

    Updating for Airflow

---
+++ dags/run_spark_once.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operaters.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="run_spark_once",
+    start_date=dtaetime(2025, 7, 30),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    run_spark = SparkSubmitOperator(
+        task_id="run_spark_transform",
+        application="/opt/etl/transformation.py",
+        conn_id="spark_default",
+        verbose=True,
+        application_args=[],
+    )
+
+    run_spark
+
+
