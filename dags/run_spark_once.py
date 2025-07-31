b551ad6f12e5914aa0360a97c5615c7deb846eed
Revanth <revanthreddypedaballi@gmail.com>
2025-07-31 15:03:42 -0500

    updating dag file

--- dags/run_spark_once.py
+++ dags/run_spark_once.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operaters.spark_submit import SparkSubmitOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 
 with DAG(
